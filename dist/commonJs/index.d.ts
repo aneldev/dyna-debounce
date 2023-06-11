2d4a21acb363f511b7bc27a512f98ecc570ed7ee
dennisat <dennis@anel.co>
2023-06-11 12:54:33 +0200

    Build

--- dist/commonJs/index.d.ts
+++ dist/commonJs/index.d.ts
@@ -2,4 +2,4 @@ export interface IDynaDebounceConfig {
     leading?: boolean;
     maxWait?: number;
 }
-export declare const dynaDebounce: (func: (args?: any[] | undefined) => any, timeout: number, config?: IDynaDebounceConfig) => any;
+export declare const dynaDebounce: (func: (...args: any[]) => any, timeout: number, config?: IDynaDebounceConfig) => any;
